b50b3eb36ff1ab4f180049a9057d95ce769b92d9
John Bretherton <270022735@bun2-204-16.local>
2019-05-30 17:04:15 +1200

    no progress

--- js/script.js
+++ js/script.js
@@ -3,10 +3,23 @@
 // var dieNumber = (Math.floor(Math.random() * 6) + 1);
 // console.log(dieNumber);
 
-var dieNumber;
+// var dieNumber;
+//
+// $('#dieButton').click(function(){
+//     var dieNumber = (Math.floor(Math.random() * 6) + 1);
+//     $('#dieNumber').text(dieNumber);
+//     console.log(dieNumber);
+// });
 
-$('#dieButton').click(function(){
-    var dieNumber = (Math.floor(Math.random() * 6) + 1);
-    $('#dieNumber').text(dieNumber);
-    console.log(dieNumber);
-});
+
+// 2. Create an application that will act as a guessing game. Ask the user to enter a number between 1 and 10 and then check to see if that is the number the computer has generated.
+
+var userNumber = parseInt(prompt("Enter a number between 1 and 10. Let's see if I can guess it!"));
+console.log(userNumber);
+
+
+
+
+
+
+// 3. Create an application for a rock, paper, scissors game versus the computer.
